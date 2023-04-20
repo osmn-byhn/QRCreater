55dd558146a23dbbdb5b723205f8449fdd5df03c
osmn-byhn <osmanbeyhan12@gmail.com>
2023-04-20 20:52:07 +0300

    commit for skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,50 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+  import { RouterLink, RouterView } from 'vue-router'
+  import { reactive } from 'vue'
+
+  function htmlEncode (value){
+  return $('<div/>').text(value).html();
+}
+
+$(function() {
+  $("#generate").click(function() {
+    $(".qr-code").attr("src", "https://chart.googleapis.com/chart?cht=qr&chl=" + htmlEncode($("#content").val()) + "&chs=160x160&chld=L|0");
+  });
+});
 </script>
 
 <template>
-  <h1>Hello World!</h1>
+  
+  <div class="container-fluid">
+  <div class="text-center">
+    <img src="https://chart.googleapis.com/chart?cht=qr&chl=Hello+World&chs=160x160&chld=L|0"
+         class="qr-code img-thumbnail img-responsive">
+  </div>
+
+  <div class="form-horizontal">
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="content">Content:</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="content" placeholder="Enter content">
+      </div>
+    </div>
+    <div class="form-group"> 
+      <div class="col-sm-offset-2 col-sm-10">
+        <button type="button" class="btn btn-default" id="generate">Generate</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
   <RouterView />
 </template>
 
 <style scoped>
-
+  #qrcode {
+    width: 256px;
+    height: 256px;
+    margin-top:15px;
+  }
 </style>
