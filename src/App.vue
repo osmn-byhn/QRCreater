d2a846b029dd46f65b9bdbf5805f1dfe6c75cfb1
osmn-byhn <osmanbeyhan12@gmail.com>
2023-05-13 12:41:55 +0300

    commit for api

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,56 @@
 <script setup>
   import { RouterLink, RouterView } from 'vue-router'
-  import { reactive } from 'vue'
+  import { reactive, ref } from 'vue'
+  import axios from 'axios'
+  const codeUrl = ref('')
+  const dataUrl = ref('')
 
-  function htmlEncode (value){
-  return $('<div/>').text(value).html();
-}
+  const config = {
+    headers: {
+      'X-Api-Key': '0IM1OGs36No1KIg69Tg75G4IiT5UvWVe8UrDaRnI'
+    }
+  }
+  var fmt = 'png'
+  function getQR() {
+    axios.get(`https://api.api-ninjas.com/v1/qrcode?data=${dataUrl._value}&format=${fmt}&size=200`, config)
+    .then((res) => {
+      console.log(dataUrl);
+      console.log(res.data)
+      codeUrl.value = `data:image/png;base64, ${res.data}`
+      //deneme.value = codeUrl._value
 
-$(function() {
-  $("#generate").click(function() {
-    $(".qr-code").attr("src", "https://chart.googleapis.com/chart?cht=qr&chl=" + htmlEncode($("#content").val()) + "&chs=160x160&chld=L|0");
-  });
-});
+    })
+    
+    .catch((err) => {
+      console.log(err)
+  })
+  }
 </script>
 
 <template>
   
   <div class="container-fluid">
-  <div class="text-center">
-    <img src="https://chart.googleapis.com/chart?cht=qr&chl=Hello+World&chs=160x160&chld=L|0"
-         class="qr-code img-thumbnail img-responsive">
+    <div class="text-center">
+      <img :src="codeUrl">
+    </div>
+
+    <div class="form-horizontal">
+      <div class="form-group">
+        <label class="control-label col-sm-2" for="content">Content:</label>
+        <div class="col-sm-10">
+          <input type="text"  v-model="dataUrl" class="form-control" id="content" placeholder="Enter content">
+        </div>
+        <p>{{ dataUrl }}</p>
+      </div>
+      <div class="form-group"> 
+        <div class="col-sm-offset-2 col-sm-10">
+          <button type="button" class="btn btn-default" id="generate" @click="getQR()">Generate</button>
+        </div>
+      </div>
+    </div>
   </div>
 
-  <div class="form-horizontal">
-    <div class="form-group">
-      <label class="control-label col-sm-2" for="content">Content:</label>
-      <div class="col-sm-10">
-        <input type="text" class="form-control" id="content" placeholder="Enter content">
-      </div>
-    </div>
-    <div class="form-group"> 
-      <div class="col-sm-offset-2 col-sm-10">
-        <button type="button" class="btn btn-default" id="generate">Generate</button>
-      </div>
-    </div>
-  </div>
-</div>
+  
 
 
 
